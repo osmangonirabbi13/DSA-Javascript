da4d5b7551e7339206f810843754fc819d2937ca
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-07-26 18:31:03 +0600

    Reverse without  extra space

--- Reverse.js
+++ Reverse.js
@@ -1,4 +1,4 @@
-// Reverse  extra space
+// Reverse with  extra space
 
 let arr = [1, 2, 3, 4, 5];
 let copy = new Array(arr.length);
@@ -7,3 +7,21 @@ for (let i = 0; i < arr.length; i++) {
   copy[i] = arr[arr.length - 1 - i];
 }
 console.log(copy);
+
+// Reverse without  extra space
+
+let arr = [1, 2, 3, 4, 5];
+
+let i = 0;
+let j = arr.length - 1;
+
+while (i < j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+
+  i++;
+  j--;
+}
+
+console.log(arr);
